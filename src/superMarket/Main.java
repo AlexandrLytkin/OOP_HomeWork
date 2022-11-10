dae13c2f45a1aadeaadd5a6a5b2b42c9a88d4329
AlexandrLytkin <Dracca@mail.ru>
2022-11-10 10:42:27 +0300

    Проверьте дз

---
+++ src/superMarket/Main.java
@@ -0,0 +1,69 @@
+package superMarket;
+
+import java.util.ArrayDeque;
+import java.util.List;
+import java.util.Queue;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Queue<String> queue1 = new ArrayDeque<>();
+        Queue<String> queue2 = new ArrayDeque<>();
+
+        randomFilling(queue1);
+        randomFilling(queue2);
+        System.out.println("queue1 = " + queue1);
+        System.out.println("queue2 = " + queue2);
+        System.out.println();
+        add("Посетитель №11", queue1,queue2);
+        System.out.println("queue1 = " + queue1);
+        System.out.println("queue2 = " + queue2);
+        System.out.println();
+        remove(queue1,queue2);
+        System.out.println("queue1 = " + queue1);
+        System.out.println("queue2 = " + queue2);
+    }
+    private static final List<String> NAMES = List.of(
+            "Петровна",
+            "Люба",
+            "Степаныч",
+            "Лешка",
+            "Владимир",
+            "Генадий",
+            "Александр",
+            "Илья",
+            "Михаил",
+            "Вовка"
+    );
+    private static final Random RANDOM = new Random();
+    private static final int MAX_SIZE = 5;
+
+    private static void add(String name, Queue<String> queue1, Queue<String> queue2) {
+        if (queue1.size() == MAX_SIZE && queue2.size() == MAX_SIZE) {
+            System.out.println("нужно позвать Галю");
+            return;
+        }
+
+        if (queue1.size() < queue2.size()) {
+            queue1.offer(name);
+        } else {
+            queue2.offer(name);
+        }
+    }
+
+    private static void remove(Queue<String> queue1, Queue<String> queue2) {
+        if (RANDOM.nextBoolean()) {
+            queue1.poll();
+        } else {
+            queue2.poll();
+        }
+    }
+
+    private static void randomFilling(Queue<String> queue) {
+        int size = RANDOM.nextInt(MAX_SIZE+1);
+        for (int i = 0; i < size; i++) {
+            queue.offer(NAMES.get(RANDOM.nextInt(NAMES.size())));
+        }
+    }
+}
